3141288a9345c2c5a066efe1330ba0661f6230c0
0x60018 <1799476517@qq.com>
2022-08-31 15:10:17 +0800

    updates

--- backend/src/schedule/jobs.ts
+++ backend/src/schedule/jobs.ts
@@ -85,7 +85,7 @@ export function jobFaucetTwitter() {
     new FaucetService().fromTwitter()
   }
 
-  // new MJobPessimism('*/5 * * * * *', callback, jobFaucetTwitter.name).schedule()
+  new MJobPessimism('*/5 * * * * *', callback, jobFaucetTwitter.name).schedule()
 }
 
 export function jobPoolCollect() {
