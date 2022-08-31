2b9482ff3eb7a4e898d0917bd8fadc84b480f2c9
yuyunhong <1162528696@qq.com>
2022-08-31 18:09:39 +0800

    add faucet

--- backend/src/config/faucet.ts
+++ backend/src/config/faucet.ts
@@ -3,12 +3,13 @@ import { ethers } from 'ethers'
 export default {
   privateKey: process.env['FAUCET_PRIVATE_KEY'],
   account: process.env['FAUCET_ACCOUNT'],
-  ethAddress: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+  ethAddress:
+    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
   ethAmount: ethers.utils.parseEther('0.01'),
   aAddress:
-    '0x021cd6f82b7a863d8b9805d391d942fe50179dc41f2dcd1620d1ad7c4bb8bab9',
-  aAmount: ethers.utils.parseEther('500'),
+    '0x01036f19169e2bf0c049a1588d63cd82eadd18e9c4798f6bfb962b5541fa7ee5',
+  aAmount: ethers.utils.parseUnits('500', 6),
   bAddress:
-    '0x002e6249b9e54016496a3e618a87537afeb6451105fb645de38b340d1faba527',
-  bAmount: ethers.utils.parseEther('500'),
+    '0x030c6217597b12be57937c9743cc2fafb4e06d1f15487cd87b7fdd787b825b5b',
+  bAmount: ethers.utils.parseUnits('500', 6),
 }
