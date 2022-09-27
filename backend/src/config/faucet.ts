fc1137bada80122de6488d2b1063b5fdd77c51ce
0x60018 <1799476517@qq.com>
2022-09-27 20:42:34 +0800

    Faucet 0.01ETH to 0.005ETH

--- backend/src/config/faucet.ts
+++ backend/src/config/faucet.ts
@@ -8,7 +8,7 @@ export default {
   accounts: FAUCET_ACCOUNTS?.split(',') || [],
   ethAddress:
     '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
-  ethAmount: ethers.utils.parseEther('0.01'),
+  ethAmount: ethers.utils.parseEther('0.005'),
   aAddress:
     '0x02e2faab2cad8ecdde5e991798673ddcc08983b872304a66e5f99fbb24e14abc',
   aAmount: ethers.utils.parseUnits('500', 6),
